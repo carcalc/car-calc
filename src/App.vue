b6feaf450e3e9da63f64c68deb77ce045d31d5ac
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-02-14 12:56:32 +0100

    New vue project with router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <AppHeader/>
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view/>
+    <AppFooter/>
   </div>
 </template>
-
+<script>
+import AppHeader from "@/components/AppHeader";
+import AppFooter from "@/components/AppFooter";
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppFooter
+  }
+};
+</script>
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ParentForm/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ParentForm from "@/components/ParentForm.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ParentForm
   }
 };
 </script>
