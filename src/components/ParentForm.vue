052f91fc8057def0513572ca1483f1dbcb4de79c
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-02-14 13:54:43 +0100

    added title via props

--- src/components/ParentForm.vue
+++ src/components/ParentForm.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="wrapper">
     <div>
-      <h1>{{ formTitleOne }}</h1>
-      <BaseForm v-on:dataToParent="childClicked1"/>
+      <BaseForm title="Bil 1" v-on:dataToParent="childClicked1"/>
     </div>
     <div>
-      <h1>{{ formTitleTwo }}</h1>
-      <BaseForm v-on:dataToParent="childClicked2"/>
+      <BaseForm title="Bil 2" v-on:dataToParent="childClicked2"/>
     </div>
     <button @click="compare" class="compareBtn">Jämför</button>
     <br>
@@ -24,8 +22,6 @@ export default {
   },
   data() {
     return {
-      formTitleOne: "Bil 1",
-      formTitleTwo: "Bil 2",
       formDataOne: "",
       formDataTwo: "",
       output: ""
@@ -42,7 +38,7 @@ export default {
     compare() {
       if (this.formDataOne > this.formDataTwo) {
         const result = this.formDataOne - this.formDataTwo;
-        this.output = `<h2>'Bil 1 är' ${result} kr dyrare än bil 2</h2>`;
+        this.output = `Bil 1 är' ${result} kr dyrare än bil 2`;
       } else {
         const result = this.formDataTwo - this.formDataOne;
         this.output = `Bil 2 är ${result} kr dyrare än bil 1`;
@@ -53,15 +49,4 @@ export default {
 </script>
 
 <style scoped>
-/* .wrapper {
-  padding: 50px;
-}
-.wrapper div {
-  width: 50%;
-  display: flex;
-}
-.compareBtn {
-  padding: 20px 70px;
-  background: lightseagreen;
-} */
 </style>
