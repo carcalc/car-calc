44335842be66e9148b5b5ab946023d8a5f4f93ad
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-02-22 21:21:56 +0100

    reset css

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,40 @@ export default {
 };
 </script>
 <style>
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
 
-header,
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
 body,
-footer {
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ol,
+ul {
   margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
 }
 
 #app {
