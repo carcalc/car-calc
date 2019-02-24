549e78288a757aa8fe40a020005612f6641c770a
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-02-24 23:13:58 +0100

    some computed functions

--- src/components/CostComparison.vue
+++ src/components/CostComparison.vue
@@ -1,30 +1,57 @@
 <template>
-  <div class="cost-comparison">
-    -här hamnar besparingar och grafer-
+  <div class="cost-comparison">-här hamnar besparingar och grafer-
     <!-- This is just bullshit output to make sure data is where it's supposed to be -->
     <!-- remove all this crap once we have proper computed properties to display -->
     <p>{{ usageDetails.distance }} körsträcka</p>
     <p>{{ usageDetails.gasPrice }} bensin/dieselpris</p>
     <p>{{ usageDetails.kwhPrice }} elpris</p>
+
     <p>Pris {{ selectedCars[0].name }}: {{ selectedCars[0].price }} kr</p>
     <p>Pris {{ selectedCars[1].name }}: {{ selectedCars[1].price }} kr</p>
+
+    <h1>{{firstYearTotalCarOne}}</h1>
+    <h1>{{firstYearTotalCarTwo}}</h1>
+    <h1>{{comparisonFirstYear}}</h1>
     <!-- Replace all of the above with output from computed properties -->
   </div>
 </template>
 
 <script>
 export default {
-  props: ['usageDetails', 'selectedCars'],
+  props: ["usageDetails", "selectedCars"],
   computed: {
     taxCredit() {
       // if possible, calculate tax credit for green cars; otherwise make sure the database has this info
-      return '';
+      return "";
     },
     pricePerKm() {
       // Put all calculations in computed properties like these
-      return '';
+      return "";
     },
-  },
+    firstYearTotalCarOne() {
+      let { kwhPrice, gasPrice, distance, ownership } = this.usageDetails;
+      let { name, price, type, consumption } = this.selectedCars[0];
+      return Math.round(
+        type === "electric"
+          ? kwhPrice * distance * consumption + price - 60000
+          : gasPrice * distance * consumption + price
+      );
+    },
+    firstYearTotalCarTwo() {
+      let { kwhPrice, gasPrice, distance, ownership } = this.usageDetails;
+      let { name, price, type, consumption } = this.selectedCars[1];
+      return Math.round(
+        type === "electric"
+          ? kwhPrice * distance * consumption + price - 60000
+          : gasPrice * distance * consumption + price
+      );
+    },
+    comparisonFirstYear() {
+      this.firstYearTotalCarOne < this.firstYearTotalCarTwo
+        ? console.log(this.firstYearTotalCarTwo - this.firstYearTotalCarOne)
+        : console.log(this.firstYearTotalCarOne - this.firstYearTotalCarTwo);
+    }
+  }
 };
 
 // Maybe reuse these calculations as Computed Properties above
