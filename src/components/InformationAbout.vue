e1e1d514cb20809380cf9798ffc8b8ef51a4660d
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-03-05 21:23:56 +0100

    added som about us text

--- src/components/InformationAbout.vue
+++ src/components/InformationAbout.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="about-us">
     <h2>Om</h2>
-    <p>tjena kuken! Här ska vi skryta om oss själva</p>
+    <p>
+      Vi som gjort denna site heter Andreas och Gabriel. Vi är i slutet på vår utbildning till
+      frontendutvecklare på Nackademin och detta är vårat examensprojekt. Vi har själva saknat en
+      site där man på ett enkelt och smidigt sätt kan jämföra kostnaderna vid köp samt ägande av el-
+      vs fossildriven bil.
+    </p>
   </section>
 </template>
 
