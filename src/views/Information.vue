d7db6d49ec73ad297cace0422722e8b829f4fa7b
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-03-09 20:57:53 +0100

    background on about text on tablet & desktop

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -19,6 +19,12 @@ export default {
   max-width: 600px;
   margin: 0 auto;
   padding: 1rem;
+  @media screen and (min-width: $size-tablet) {
+    padding: 3rem;
+    background-color: rgba(255, 255, 255, 0.1);
+    box-shadow: 1px 1px 2px 0px rgba(0, 0, 80, 0.3);
+    border-radius: $border-radius;
+  }
 
   h2,
   p {
