f891106404de4f4ce65986420c5b1c29f3a7bb55
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-03-13 12:17:51 +0100

    changed the car-logo func

--- src/components/CarLogos.vue
+++ src/components/CarLogos.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="car-logo">
-    <img :src="carLogo()" alt="Car Logo" />
-  </div>
+  <img :src="carName" alt="Car Logo" />
 </template>
 
 <script>
@@ -13,65 +11,23 @@ export default {
   data() {
     return {};
   },
-  methods: {
-    carLogo() {
-      const { name } = this.car;
-
-      switch (true) {
-        case name.includes('Mercedes'):
-          return require('../assets/mercedes.svg');
-        case name.includes('Volvo'):
-          return require('../assets/volvo.svg');
-        case name.includes('Kia'):
-          return require('../assets/kia.svg');
-        case name.includes('Nissan'):
-          return require('../assets/nissan.svg');
-        case name.includes('Volkswagen'):
-          return require('../assets/volkswagen.svg');
-        case name.includes('Tesla'):
-          return require('../assets/tesla.svg');
-        case name.includes('Citro'):
-          return require('../assets/citroen.svg');
-        case name.includes('Peugeot'):
-          return require('../assets/peugeot.svg');
-        case name.includes('Renault'):
-          return require('../assets/renault.svg');
-        case name.includes('Smart'):
-          return require('../assets/smart-car.svg');
-        case name.includes('Jeep'):
-          return require('../assets/jeep.svg');
-        case name.includes('Hyundai'):
-          return require('../assets/hyundai.svg');
-        case name.includes('Opel'):
-          return require('../assets/opel.svg');
-        case name.includes('Ford'):
-          return require('../assets/ford.svg');
-        case name.includes('Fiat'):
-          return require('../assets/fiat.svg');
-        case name.includes('BMW'):
-          return require('../assets/bmw.svg');
-        case name.includes('Alfa Romeo'):
-          return require('../assets/alfa-romeo.svg');
-        case name.includes('Audi'):
-          return require('../assets/audi.svg');
-        default:
-          return require('../assets/default-car-logo.svg');
-      }
+  computed: {
+    carName: function() {
+      const name = this.car.name.split(' ')[0].toLowerCase();
+      return require('../assets/' + name + '.svg');
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-.car-logo {
-  img {
-    height: 1.1rem;
-    position: absolute;
-    right: 35px;
-    top: 10px;
-    @media screen and (min-width: $size-tablet) {
-      height: 2.5rem;
-      left: 10px;
-    }
+img {
+  height: 1.1rem;
+  position: absolute;
+  right: 35px;
+  top: 10px;
+  @media screen and (min-width: $size-tablet) {
+    height: 2.5rem;
+    left: 10px;
   }
 }
 </style>
