25782ede8c564d5cdfc2d9e95b534f0c377cd025
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2019-03-13 09:51:28 +0100

    car-logos

---
+++ src/components/CarLogos.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <img :src="carLogo()" alt="Car Logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarLogos',
+  props: {
+    car: { type: Object, required: true },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    carLogo() {
+      const { name } = this.car;
+
+      switch (true) {
+        case name.includes('Mercedes'):
+          return require('../assets/mercedes.svg');
+        case name.includes('Volvo'):
+          return require('../assets/volvo.svg');
+        case name.includes('Kia'):
+          return require('../assets/kia.svg');
+        case name.includes('Nissan'):
+          return require('../assets/nissan.svg');
+        case name.includes('Volkswagen'):
+          return require('../assets/volkswagen.svg');
+        case name.includes('Tesla'):
+          return require('../assets/tesla.svg');
+        case name.includes('Citro'):
+          return require('../assets/citroen.svg');
+        case name.includes('Peugeot'):
+          return require('../assets/peugeot.svg');
+        case name.includes('Renault'):
+          return require('../assets/renault.svg');
+        case name.includes('Smart'):
+          return require('../assets/smart-car.svg');
+        case name.includes('Jeep'):
+          return require('../assets/jeep.svg');
+        case name.includes('Hyundai'):
+          return require('../assets/hyundai.svg');
+        case name.includes('Opel'):
+          return require('../assets/opel.svg');
+        case name.includes('Ford'):
+          return require('../assets/ford.svg');
+        case name.includes('Fiat'):
+          return require('../assets/fiat.svg');
+        case name.includes('BMW'):
+          return require('../assets/bmw.svg');
+        case name.includes('Alfa Romeo'):
+          return require('../assets/alfa-romeo.svg');
+        case name.includes('Audi'):
+          return require('../assets/audi.svg');
+        default:
+          return require('../assets/default-car-logo.svg');
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+img {
+  width: 2.5rem;
+  position: absolute;
+  left: 10px;
+  top: 10px;
+}
+</style>
