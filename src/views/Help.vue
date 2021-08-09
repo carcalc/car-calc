9d97ab7fca83d9a2022c3e45dd081d829900c8ec
Gabriel Lundmark <g.lundmark@gmail.com>
2021-08-09 22:33:51 +0200

    refactor: remove useless helptext component

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="help">
-    <HelpText />
+    <h2>Hjälp</h2>
+    <p>
+      Jämför enkelt kostnaderna mellan två bilar genom att ändra uppgifterna som vi har förifyllt.
+      Du kan också välja en bil från menyn och ändra dess specifikationer.
+    </p>
+    <p>
+      Fyll i uppgifter som berör ägande och drift i rutan i mitten. Dessa gäller för båda bilarna
+      och de förifyllda värdena går förstås bra att ändra vid behov. I rutan längst ned ser du sedan
+      resultatet av jämförelsen.
+    </p>
   </div>
 </template>
-<script>
-import HelpText from '@/components/HelpText.vue';
-
-export default {
-  name: 'help',
-  components: {
-    HelpText,
-  },
-};
-</script>
 
 <style lang="scss">
 .help {
