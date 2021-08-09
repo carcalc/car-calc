832f957178d85bdbd6219d8698778c01a6581f15
Gabriel Lundmark <g.lundmark@gmail.com>
2021-08-09 22:14:37 +0200

    feat: reword about text

--- src/components/AboutText.vue
+++ src/components/AboutText.vue
@@ -2,11 +2,11 @@
   <section class="about-text">
     <h2>Om CarCalc</h2>
     <p>
-      Vi tror att drivkraften bakom övergången till fossilfria transporter främst kommer att vara
-      ekonomiska, även om det finns många andra goda skäl till att köra elektriskt. Därför vill vi
-      göra det enkelt att räkna ut besparingar i driftkostnad och ställa dem i relation till
-      elbilens (för närvarande) högre inköpspris. Förhoppningsvis kan en inblick i totalkostnaden
-      motivera fler människor att köra miljövänligt!
+      Vi tror att drivkraften bakom övergången till fossilfria transporter främst är ekonomiska,
+      även om det finns många andra goda skäl till att köra elektriskt. Därför vill vi göra det
+      enkelt att räkna ut besparingar i driftkostnad och ställa dem i relation till elbilens högre
+      inköpspris. Förhoppningsvis kan en inblick i totalkostnaden motivera fler människor att köra
+      miljövänligt!
     </p>
     <h2>Om oss</h2>
     <p>
@@ -25,23 +25,21 @@
 
     <h2>Beräkningar</h2>
     <p>
-      Driftkostnaden är betydligt lägre för elbilar än traditionella bilar och mycket pekar på att
-      så även är fallet med värdeminskning, underhåll<sup>1</sup> och skatt, men detta är svåra och
-      i bästa fall imprecisa beräkningar som vi för närvarande valt att utelämna.
-      Miljöbilspremien<sup>2</sup> om 60 000 kr som betalas ut till köpare av nya elbilar är
-      medräknad där det är relevant.
+      Driftkostnad och skatt är betydligt lägre för elbilar än bilar med förbränningsmotor och
+      mycket talar för att så även är fallet underhåll<sup>1</sup>, men det är en svår och imprecis
+      beräkning att uppskatta, så vi har förnärvarande valt att utelämna den. Det kan inte
+      garanteras att den data som presenteras bland exempelbilarna är 100% korrekt. Vi
+      tillhandahåller en lista på vanliga bilar att utgå ifrån, men uppmanar även till att göra
+      justeringar vid behov genom att mata in data från t.ex. din nuvarande bils faktiska
+      förbrukning. På grund av de många variabler som påverkar en bils totala ägandekostnad ska
+      siffrorna ses som väglendande snarare än en absolut sanning. Vi kan omöjligen veta exakt.
+      <br />
+      Miljöbilspremien<sup>2</sup>
+      som betalas ut till köpare av nya elbilar är medräknad där det är relevant och så länge boxen
+      är ikryssad.
     </p>
     <h2>Källor</h2>
-    <p>
-      Även om vi försöker ange korrekta biluppgifter, så kan det inte garanteras att den data som
-      presenteras är hundra procent korrekt. Vi tillhandahåller en lista på vanliga bilar att utgå
-      ifrån, men uppmanar även till att göra justeringar vid behov.
-    </p>
-    <p>
-      Klimatindex samt övriga specifikationer på bilarna i databasen kommer främst från
-      bilsvar.se<sup>3</sup>. Bilarnas förbrukning avser blandad körning. I beräkningarna antas det
-      att man som bilägare av elbil betalar 100% av bilens laddning.
-    </p>
+
     <ol class="references">
       <li>
         <a target="_blank" :href="references.elbilsnytt">Elbilsnytt</a>
