0cb75a5502ac223c1bef63901e61409aec368938
Gabriel Lundmark <g.lundmark@gmail.com>
2021-08-12 11:08:38 +0200

    refactor: sass setup

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,7 @@ module.exports = {
     loaderOptions: {
       sass: {
         additionalData: `
-          @import "@/scss/_variables.scss";
-          @import "@/scss/_mixins.scss";
-          @import "@/scss/_reset.scss";
+          @import "@/scss/main.scss";
         `,
       },
     },
